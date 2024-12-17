243e017739ae9e7656b1c2be067621d6af277c43
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-17 21:39:35 +0100

    image

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import kebulania from '../assets/kebulania.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        <img className="h-15 w-20 ml-4" src={kebulania} alt='kebulania'/>
+    </div>
+  )
+}
+
+export default Navbar
