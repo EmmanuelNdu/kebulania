69f1818506d53c24856a02e859ef3f47bf99b1e6
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-18 01:38:47 +0100

    button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import kebulania from '../assets/kebulania.png'
+import { navItems } from '../constants'
 
 const Navbar = () => {
   return (
     <div>
+    <div className='flex'>
         <img className="h-15 w-20 ml-4" src={kebulania} alt='kebulania'/>
+        <ul className=''>
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <a href={item.href}>
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+    </div>
+    <div>
+      <a href='#' className='py-2 px-3 border rounded-md'>
+        INVEST NOW
+        </a>
+        <a href='#' className='py-2 px-3 border rounded-md'>
+          JOIN US
+        </a>
+    </div>
     </div>
   )
 }
---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+ export const navItems = [
+    {label: "ABOUT", href: "#"},
+    {label: "FEATURES", href: "#"},
+    {label: "IMPACT", href: "#"},
+    {label: "GALLERY", href: "#"},
+    {label: "CONTACT", href: "#"}
+ ];
+
+ export const navItems_m = [
+    {label: "ABOUT", href: "#"},
+    {label: "FEATURES", href: "#"},
+    {label: "IMPACT", href: "#"},
+    {label: "GALLERY", href: "#"},
+    {label: "CONTACT", href: "#"},
+ ];
