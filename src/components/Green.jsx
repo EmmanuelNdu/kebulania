a25f96e25f36a4d4529c70f1648b75e88df82e1e
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-20 18:47:23 +0100

    cleaning

--- src/components/Green.jsx
+++ src/components/Green.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Star} from "lucide-react"
+import shapestar from '../assets/Shapestar.png'
 
 const Green = () => {
   return (
     <div className='bg-[#1D363C] p-16'>
-      <div className='flex flex-row justify-center'>
-      <Star color='yellow'/>
-      <Star color='yellow'/>
-      <Star color='yellow'/>
-      <Star color='yellow'/>
-      <Star color='yellow'/>
+      <div className='flex flex-row justify-center space-x-2'>
+      < img src={shapestar} />
+      < img src={shapestar} />
+      < img src={shapestar} />
+      < img src={shapestar} />
+      < img src={shapestar} />
       </div>
-      <p className='flex justify-center text-[white] text-4xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed<br/>
+      <p className='flex justify-center text-[white] text-4xl text-center pt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed<br/>
        do eiusmod tempor incididunt ut labore et dolore magna<br/>
         consectetur aliqua</p>
     </div>
