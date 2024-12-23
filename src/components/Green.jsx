189426f5b1dbad17890bb213a78f296679320461
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-12-24 00:26:45 +0100

    gov

--- src/components/Green.jsx
+++ src/components/Green.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import shapestar from '../assets/Shapestar.png'
+import gov from '../assets/gov.png'
 
 const Green = () => {
   return (
@@ -11,9 +12,16 @@ const Green = () => {
       < img src={shapestar} />
       < img src={shapestar} />
       </div>
-      <p className='flex justify-center text-[white] text-4xl text-center pt-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed<br/>
-       do eiusmod tempor incididunt ut labore et dolore magna<br/>
-        consectetur aliqua</p>
+      <div className='flex flex-col items-center space-y-6'>
+      <p className=' text-[white] text-[40px] text-center pt-6 font-bold'>“Lagos Film City (Kebulania) represents Lagos State's<br/>
+            vision for creative excellence and technological <br/>
+            innovation. This investment will transform our creative<br/>
+            landscape while creating groundbreaking opportunities <br/>
+            for our talented youth.”</p>
+        <img className='w-156 h-156' src={gov} />
+        <p className='font-semibold text-2xl text-[#ffffff]'>His Excellency, Babajide Sanwo-Olu,<br/>
+        <span className='font-light text-2xl text-[#ffffff]'>the Executive Governor of Lagos State</span></p>
+    </div>
     </div>
   )
 }
