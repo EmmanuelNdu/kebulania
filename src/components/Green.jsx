a31f4f5e0ac6cd1d5b98a6ffb7a38f106743b0cc
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-01-19 20:57:33 +0100

    kj

--- src/components/Green.jsx
+++ src/components/Green.jsx
@@ -14,7 +14,7 @@ const Green = () => {
       </div>
       <div className='flex flex-col items-center space-y-6'>
       <p className=' text-[white] text-[40px] text-center pt-6 font-bold'>“Lagos Film City (Kebulania) represents Lagos State's<br/>
-            vision for creative excellence and technological <br/>
+            vision for creative excellence and technological  <br/>
             innovation. This investment will transform our creative<br/>
             landscape while creating groundbreaking opportunities <br/>
             for our talented youth.”</p>
