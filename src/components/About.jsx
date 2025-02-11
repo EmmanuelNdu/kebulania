e0158ef8076a956ba7db103b508e3b30178ca290
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-02-11 23:06:14 +0100

    l

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <div className='text-[#C3C9D1] flex-1'>
         <h1 className='text-[#E5C778] font-medium text-4xl tracking-widest pt-14'>ABOUT KEBULANIA</h1>
         <p className='pt-6 text-[#C3C9D1]'>On the outskirts of Lagos, a revolution in creative excellence is taking shape.<br/>
-        Kebulania represents Africa's bold entry into global creative and technological <br/>
+        Kebulania represents Africa's bold qentry into global creative and technological <br/>
         leadership, combining world-class infrastructure with unmatched opportunity for<br/>
         talent development. This landmark development creates new possibilities for African<br/>
         creators while establishing Nigeria as a premier destination for global creative <br/>
